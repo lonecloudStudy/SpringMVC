d47e466f081d499d55a54fb411919911f4e43a3d
lonecloud <lonecloudmac@icloud.com>
2017-01-11 13:41:25 +0800

    初始化

---
+++ src/main/java/cn/lonecloud/controller/HelloController.java
@@ -0,0 +1,22 @@
+package cn.lonecloud.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		// 添加模型数据 可以是任意的POJO对象 
+		mv.addObject("message", "Hello World!");
+		//设置逻辑视图名,视图解析器会根据该名字解析到具体的视图页面 
+		mv.setViewName("hello");
+		return mv;
+	}
+
+}
