ce0806f798589a2b3db24153fe694e065de80ad7
lonecloud <lonecloudmac@icloud.com>
2017-04-23 13:19:03 +0800

    ip相关

--- src/main/java/cn/lonecloud/controller/EmployeeController.java
+++ src/main/java/cn/lonecloud/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package cn.lonecloud.controller;
 
 import cn.lonecloud.dao.EmployeeDao;
 import cn.lonecloud.entity.Employee;
+import cn.lonecloud.util.IPUtils;
 import cn.lonecloud.util.RandomUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -12,30 +13,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.Servlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
  * Created by lonecloud on 17/4/16.
+ *
  * @author lonecloud
  */
 @Controller
 @RequestMapping("/emp")
 public class EmployeeController {
 
-    private Log logger= LogFactory.getLog(EmployeeController.class);
+    private Log logger = LogFactory.getLog(EmployeeController.class);
 
     @Resource
     private EmployeeDao employeeDao;
 
     /**
      * 防止一些未填写的属性被清空
-     * @param id id
+     *
+     * @param id    id
      * @param model model
      */
     @ModelAttribute
-    public void getModel(@RequestParam("id") String id, Model model){
-        if (null!=id){
-            model.addAttribute("employee",employeeDao.queryById(id));
+    public void getModel(@RequestParam(value = "id",required = false) String id, Model model) {
+        if (null != id) {
+            model.addAttribute("employee", employeeDao.queryById(id));
         }
     }
 
@@ -47,7 +53,7 @@ public class EmployeeController {
 
     @RequestMapping("/edit/{id}")
     public String edit(@PathVariable("id") String id, Model model) {
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug(id);
         }
         if (id != null) {
@@ -56,61 +62,72 @@ public class EmployeeController {
         }
         return "edit";
     }
+
     @RequestMapping("/show/{id}")
-    public String show(@PathVariable("id") String id){
+    public String show(@PathVariable("id") String id) {
         return "list";
     }
 
-    @RequestMapping(value = "/update/",method = RequestMethod.PUT)
-    public String update(Employee employee){
+    @RequestMapping(value = "/update/", method = RequestMethod.PUT)
+    public String update(Employee employee) {
         employeeDao.update(employee);
         return "redirect:/emp/list";
     }
 
     /**
      * 显示添加员工的界面
+     *
      * @param model model
      * @return view
      */
-    @RequestMapping(value = "/emp",method = RequestMethod.GET)
-    public String empShow(Model model){
+    @RequestMapping(value = "/emp", method = RequestMethod.GET)
+    public String empShow(Model model) {
         //不管需不需要如果使用了form标签必须加上
-        model.addAttribute("employee",new Employee());
+        model.addAttribute("employee", new Employee());
         return "input";
     }
 
     /**
      * 修改员工的界面
+     *
      * @param employee 人员参数
      * @return view
      */
-    @RequestMapping(value = "/emp",method = RequestMethod.PUT)
-    public String empUpdate(Employee employee){
+    @RequestMapping(value = "/emp", method = RequestMethod.PUT)
+    public String empUpdate(Employee employee) {
         employeeDao.update(employee);
         return "redirect:/emp/list";
     }
 
     /**
      * 删除员工的界面
+     *
      * @param id id
      * @return view
      */
-    @RequestMapping(value = "/emp/{id}",method=RequestMethod.DELETE)
-    public String empDelete(@PathVariable("id") String id){
-        logger.debug("delete->>>>>>>>>"+id);
+    @RequestMapping(value = "/emp/{id}", method = RequestMethod.DELETE)
+    public String empDelete(@PathVariable("id") String id) {
+        logger.debug("delete->>>>>>>>>" + id);
         employeeDao.deleteById(id);
         return "redirect:/emp/list";
     }
 
     /**
      * 添加一个人员
+     *
      * @param employee 人员
      * @return view
      */
-    @RequestMapping(value = "/emp",method=RequestMethod.POST)
-    public String empAdd(Employee employee){
+    @RequestMapping(value = "/emp", method = RequestMethod.POST)
+    public String empAdd(Employee employee) {
         employee.setId(RandomUtils.getUUID());
         employeeDao.insert(employee);
         return "redirect:/emp/list";
+    }
+
+    @ResponseBody
+    @RequestMapping("/ip")
+    public String getIP(HttpServletRequest request) {
+        return IPUtils.getIPAddr(request);
     }
 }
